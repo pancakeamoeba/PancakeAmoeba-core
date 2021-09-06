390ff3283e732942a8b328fe3a93078f4eb8cb24
Pancake Amoeba <87421740+pancakeamoeba@users.noreply.github.com>
2021-09-06 13:43:54 +0700

    update

--- contracts/interfaces/IVaultController.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.6.12;
-
-interface IVaultController {
-    function minter() external view returns (address);
-    function stakingToken() external view returns (address);
-}
